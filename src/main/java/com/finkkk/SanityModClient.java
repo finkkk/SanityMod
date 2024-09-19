2e09d1b1a4c1922b82db9b9556c9cf720f630cfb
finkkk <flx2217183968@gmail.com>
2024-09-19 11:06:58 +0800

    1

---
+++ src/main/java/com/finkkk/SanityModClient.java
@@ -0,0 +1,10 @@
+package com.finkkk;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SanityModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/finkkk/SanityModDataGenerator.java
@@ -0,0 +1,11 @@
+package com.finkkk;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class SanityModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}

45ce07b9a31982a9d882979d0bcdadfe8f2fccbf
finkkk <flx2217183968@gmail.com>
2024-09-19 11:11:30 +0800

    2

--- src/main/java/com/finkkk/SanityMod.java
+++ src/main/java/com/finkkk/SanityMod.java
@@ -18,6 +18,7 @@ public class SanityMod implements ModInitializer {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
+		//test
 
 		LOGGER.info("Hello Fabric world!");
 	}
